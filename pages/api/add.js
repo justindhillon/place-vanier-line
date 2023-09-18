1b6c716576b6529e85b8022fb2c97cac1896010f
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 14:49:15 -0700

    modified:   pages/api/add.js

--- pages/api/add.js
+++ pages/api/add.js
@@ -11,7 +11,7 @@ function getCurrentTime(separator=''){
 }
  
 export default async function handler(request, response) {
-  await sql`INSERT INTO Database (Name, Owner) VALUES (${getCurrentTime()}, ${getCurrentTime()});`;
+  await sql`INSERT INTO Database (Time, Value) VALUES (${getCurrentTime()}, ${getCurrentTime()});`;
   const pets = await sql`SELECT * FROM Database;`;
   return response.status(200).json({ pets });
 }
