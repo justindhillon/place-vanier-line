f393db3ef0cc3affe63c01643323b7d7813aa5df
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 14:06:53 -0700

    renamed:    json/data.json -> data/data.json 	modified:   pages/api/myform.js

--- pages/api/myform.js
+++ pages/api/myform.js
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 export default function handler(req, res) {
     const body = req.body;
     
-    fs.writeFileSync("json/data.json", { user: `${body.firstName} ${body.lastName}` });
+    fs.writeFileSync("data/data.txt", { user: `${body.firstName} ${body.lastName}` });
 
     res.status(200).json({ user: `${body.firstName} ${body.lastName}` });
   }
