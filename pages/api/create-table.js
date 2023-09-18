665bf6897979c9e769871082582b3ee7aa6babbf
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 14:23:57 -0700

    new file:   pages/api/create-table.js

---
+++ pages/api/create-table.js
@@ -0,0 +1,11 @@
+import { sql } from '@vercel/postgres';
+ 
+export default async function handler(request, response) {
+  try {
+    const result =
+      await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
+    return response.status(200).json({ result });
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
+}
