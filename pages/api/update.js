e6b9956b7893362735fbd6fdcfb4abdfd4a677e3
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 16:07:50 -0700

    new file:   pages/api/update.js

---
+++ pages/api/update.js
@@ -0,0 +1,13 @@
+import { sql } from '@vercel/postgres';
+ 
+export default async function handler(request, response) {
+    for (let hour = 0; hour < 24; hour++) {
+        for (let minute = 0; minute < 60; minute += 15) {
+            await sql`INSERT INTO Timebase (Time, Value) VALUES (${hour.toString() + ":" + minute.toString()}, ${0});`;
+        }
+    }
+
+    await sql`INSERT INTO Timebase (Time, Value) VALUES (${getCurrentTime()}, ${getCurrentTime()});`;
+    const pets = await sql`SELECT * FROM Timebase;`;
+    return response.status(200).json({ pets });
+}
