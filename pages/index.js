1433b4c18090fd766bb21ad85171a9ce274919b6
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 13:52:16 -0700

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,71 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import { getSortedPostsData } from '../lib/posts';
 import React from 'react';
-import useSWR from 'swr';
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
+export default function Test() {
+  const [candidat, setCandidat] = useState({
+    lastName: "",
+    firstName: "",
+  });
+  const [user,setUser]= useState()
 
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (candidat.lastName === "" || candidat.firstName === "")
+      return alert("last name or first name is empty");
 
-      <Button />
-
-    </Layout>
-  );
-}
-
-function Button() {
-  async function fetchData() {
-    const { data, error } = await useSWR('/api/readFile', fetcher);
-    return data;
-  }
-
-  function handleClick(event) {
-    event.preventDefault();
-    fetchData();
-  }
-
-  function getCurrentTime(separator=''){
-    let newDate = new Date()
-    let hour = newDate.getHours();
-    let minute = newDate.getMinutes();
-
-    minute = (Math.round(minute/15) * 15) % 60;
-    
-    return `${hour}${separator}${minute}`
-  }
-
+await fetch("/api/myform", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(candidat),
+    }).then(res=>res.json()).then(data=>setUser(data.user))
+  };
   return (
     <div>
-      <button onClick={handleClick}>Click Me</button>
+      <Head>
+        <title> Next.js submit form</title>
+      </Head>
+
+      <form
+        onSubmit={onSubmit}
+        className="w-1/3 justify-center border-2 flex flex-col gap-4 m-4 p-2"
+     
+      >
+        <label htmlFor="Last name">Last name</label>
+        <input
+          className="border-2 border-gray-200  p-2"
+          onChange={() => {
+            setCandidat({ ...candidat, lastName: event.target.value });
+          }}
+        ></input>
+
+        <label htmlFor="First name">First name</label>
+        <input
+          className="border-2 border-gray-200  p-2"
+          onChange={() => {
+            setCandidat({ ...candidat, firstName: event.target.value });
+          }}
+        ></input>
+
+        <button
+          className="bg-black text-white text-sm font-medium p-2 rounded "
+          type="submit"
+        >
+          <>Submit</>
+        </button>
+      </form>
+      <div className="p-2">{user ? 'user is : '+ user:''}</div>
     </div>
   );
 }
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
+function getCurrentTime(separator=''){
+  let newDate = new Date()
+  let hour = newDate.getHours();
+  let minute = newDate.getMinutes();
+
+  minute = (Math.round(minute/15) * 15) % 60;
+  
+  return `${hour}${separator}${minute}`
 }
