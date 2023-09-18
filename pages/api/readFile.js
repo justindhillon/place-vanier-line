2b8079b8440ffe2481a206fa165a35694ee2c23e
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 11:36:01 -0700

    deleted:    data/data.txt 	deleted:    pages/api/hello.js 	modified:   pages/api/readFile.js 	modified:   pages/index.js

--- pages/api/readFile.js
+++ pages/api/readFile.js
@@ -1,6 +1,11 @@
-import fs from 'fs';
+import path from 'path';
+import { promises as fs } from 'fs';
 
-export default async function handler (req, res) {
-    const fileContent = await fs.readFileSync('data/data.txt', 'utf-8');
-    res.status(200).json(fileContent);
-};
+export default async function handler(req, res) {
+  //Find the absolute path of the json directory
+  const jsonDirectory = path.join(process.cwd(), 'json');
+  //Read the json data file data.json
+  const fileContents = await fs.readFile(jsonDirectory + '/data.json', 'utf8');
+  //Return the content of the data file in json format
+  res.status(200).json(fileContents);
+}
