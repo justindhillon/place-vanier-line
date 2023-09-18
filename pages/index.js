0d1b4297701e99608a6f1f8b7520cc7b041b8af3
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 11:59:39 -0700

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
-import { getSortedPostsData } from '../lib/posts';
 import React from 'react';
 import useSWR from 'swr';
 
