e69a2d63400e7cc140d5cdc435ead99dbd90f617
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 14:05:04 -0700

    modified:   json/data.json 	modified:   pages/api/myform.js

--- pages/api/myform.js
+++ pages/api/myform.js
@@ -1,5 +1,9 @@
+import { promises as fs } from 'fs';
+
 export default function handler(req, res) {
     const body = req.body;
-  
+    
+    fs.writeFileSync("json/data.json", { user: `${body.firstName} ${body.lastName}` });
+
     res.status(200).json({ user: `${body.firstName} ${body.lastName}` });
   }
