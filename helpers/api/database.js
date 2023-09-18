26d817296060300f2fcde83b840624ae59d920ba
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 10:29:58 -0700

    new file:   helpers/api/database.js

---
+++ helpers/api/database.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+export const database = {
+    readFile,
+    writeFile
+};
+
+function readFile() {
+    let data = fs.readFileSync("data/data.txt").toString();
+    return data;
+}
+
+function writeFile(data) {
+    fs.writeFileSync("data/data.txt", data);
+}
