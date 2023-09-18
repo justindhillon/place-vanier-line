91f83633279ce3bb4702295cc8ef44a22d627b46
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 11:02:04 -0700

    modified:   pages/api/readFile.js

--- pages/api/readFile.js
+++ pages/api/readFile.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-export default (req, res) => {
+export async function Post (req, res) {
     const fileContent = fs.readFileSync('data/data.txt', 'utf-8');
     res.status(200).json(fileContent);
 };
