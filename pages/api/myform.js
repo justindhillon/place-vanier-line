09e89529300778e8528640f8c2ed549ef08c90a7
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 13:58:07 -0700

    modified:   pages/api/myform.js

--- pages/api/myform.js
+++ pages/api/myform.js
@@ -1,6 +1,16 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+
 export default function handler(req, res) {
-    const body = req.body;
+    //const body = req.body;
   
-    res.status(200).json({ user: `${body.firstName} ${body.lastName}` });
+    //res.status(200).json({ user: `${body.firstName} ${body.lastName}` });
+
+    //Find the absolute path of the json directory
+    const jsonDirectory = path.join(process.cwd(), 'json');
+    //Read the json data file data.json
+    const fileContents = fs.readFile(jsonDirectory + '/data.json', 'utf8');
+    //Return the content of the data file in json format
+    res.status(200).json(fileContents);
   }
   
