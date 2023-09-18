c79702c9207321699fd01037cbfcc5e26eb9e50b
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-18 11:05:35 -0700

    modified:   pages/api/readFile.js 	deleted:    pages/api/staticdata.js

--- pages/api/readFile.js
+++ pages/api/readFile.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-export async function handler (req, res) {
+export default async function handler (req, res) {
     const fileContent = await fs.readFileSync('data/data.txt', 'utf-8');
     res.status(200).json(fileContent);
 };
