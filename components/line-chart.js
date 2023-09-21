8103594f39952b46c70439e239cecb78e9b3f974
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-21 15:06:17 -0700

    modified:   components/line-chart.js

--- components/line-chart.js
+++ components/line-chart.js
@@ -2,6 +2,43 @@ import React from "react";
 import { Line } from 'react-chartjs-2'
 import { Chart as ChartJS } from 'chart.js/auto'
 
-export default function LineChart ({ data }) {
-    return <Line data={data} />
+const UserData = [
+    {
+      id: 1,
+      year: 2016,
+      userGain: 80000,
+      userLost: 823,
+    },
+    {
+      id: 2,
+      year: 2017,
+      userGain: 45677,
+      userLost: 345,
+    },
+    {
+      id: 3,
+      year: 2018,
+      userGain: 78888,
+      userLost: 555,
+    },
+    {
+      id: 4,
+      year: 2019,
+      userGain: 90000,
+      userLost: 4555,
+    },
+    {
+      id: 5,
+      year: 2020,
+      userGain: 4300,
+      userLost: 234,
+    },
+  ];
+
+export default function LineChart () {
+    fetch('https://placevanierline.vercel.app/api/get-data', { method: 'POST' })
+    .then(response => response.json())
+    .then(response => console.log(response.pets.rows)); 
+
+    return <Line data={UserData} />
 }
--- pages/index.js
+++ pages/index.js
@@ -2,39 +2,6 @@ import { useState } from "react";
 import Button from "../components/button";
 import LineChart from "../components/line-chart";
 
-const UserData = [
-  {
-    id: 1,
-    year: 2016,
-    userGain: 80000,
-    userLost: 823,
-  },
-  {
-    id: 2,
-    year: 2017,
-    userGain: 45677,
-    userLost: 345,
-  },
-  {
-    id: 3,
-    year: 2018,
-    userGain: 78888,
-    userLost: 555,
-  },
-  {
-    id: 4,
-    year: 2019,
-    userGain: 90000,
-    userLost: 4555,
-  },
-  {
-    id: 5,
-    year: 2020,
-    userGain: 4300,
-    userLost: 234,
-  },
-];
-
 export default function Homepage() {
   const [userData, setUserData] = useState({
     labels: UserData.map((data) => data.year),
@@ -55,14 +22,10 @@ export default function Homepage() {
     ],
   });
 
-  fetch('https://placevanierline.vercel.app/api/get-data', { method: 'POST' })
-    .then(response => response.json())
-    .then(response => console.log(response.pets.rows)); 
-
   return (
     <div>
       <Button />
-      <LineChart data={userData} />
+      <LineChart />
     </div>
   );
 }
