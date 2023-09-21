3eecfcc876b03ce5e7191215cd4b126edfe670dc
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-21 11:59:53 -0700

    modified:   pages/api/create-database.js

--- pages/api/create-database.js
+++ pages/api/create-database.js
@@ -1,3 +1,4 @@
+/*
 import { sql } from '@vercel/postgres';
 
 export default async function handler(request, response) {
@@ -10,3 +11,4 @@ export default async function handler(request, response) {
     const pets = await sql`SELECT * FROM Database;`;
     return response.status(200).json({ pets });
 }
+*/
