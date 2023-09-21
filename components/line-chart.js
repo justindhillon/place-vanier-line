07f0d7c4c7504f0957f16e3dd6a782dcb8c096f3
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-21 12:53:26 -0700

    modified:   components/line-chart.js

--- components/line-chart.js
+++ components/line-chart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
-import { Chart } from "chart.js";
+import { Chart as ChartJS } from "chart.js";
 
 export default function LineChart ({ data }) {
     return <Line data={data} />
