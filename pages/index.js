6df1ba51438d86ed52311049ab660e5e6973a7d6
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-21 14:43:46 -0700

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,7 @@ export default function Homepage() {
     .then(response => response.json())
     .then(response => raw_data = JSON.stringify(response))
 
-  console.log(raw_data);
+  console.log(fetch('https://placevanierline.vercel.app/api/get-data'));
 
   return (
     <div>
