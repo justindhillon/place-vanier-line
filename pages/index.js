e641ebd145485ec5aeacec7036be8c1c7185d1fe
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-21 14:26:48 -0700

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,7 @@ export default function Homepage() {
     .then(response => response.json())
     .then(response => raw_data = JSON.stringify(response))
 
-  console.log(raw_data);
+  console.log(raw_data.pets.rows);
 
   return (
     <div>
