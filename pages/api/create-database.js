d865889da7658ed2764896aed189f81815eef22c
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-21 11:55:17 -0700

    new file:   pages/api/create-database.js

---
+++ pages/api/create-database.js
@@ -0,0 +1,12 @@
+import { sql } from '@vercel/postgres';
+
+export default async function handler(request, response) {
+    for (let hour = 0; hour < 24; hour++) {
+        for (let minute = 0; minute < 60; minute += 15) {
+            await sql`INSERT INTO Megabase (Time, Value) VALUES (${hour.toString() + ":" + minute.toString()}, ${0});`;
+        }
+    }
+
+    const pets = await sql`SELECT * FROM Megabase;`;
+    return response.status(200).json({ pets });
+}
