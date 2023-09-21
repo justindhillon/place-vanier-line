4382cef166b50ccfde96e6d0db0bac9890b7685e
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-21 12:56:23 -0700

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import LineChart from "../components/line-chart";
 
-const data = [
+const UserData = [
   {
     id: 1,
     year: 2016,
@@ -34,6 +35,25 @@ const data = [
 ];
 
 export default function Homepage() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
   return (
     <div>
       <Button />
