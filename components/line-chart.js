615b4e53a2d1e89b5f597ad01e6c2c9a82b21512
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-21 15:09:34 -0700

    modified:   components/line-chart.js 	modified:   pages/index.js

--- components/line-chart.js
+++ components/line-chart.js
@@ -40,5 +40,24 @@ export default function LineChart () {
         },
       ];
 
+      const [userData, setUserData] = useState({
+        labels: UserData.map((data) => data.year),
+        datasets: [
+          {
+            label: "Users Gained",
+            data: UserData.map((data) => data.userGain),
+            backgroundColor: [
+              "rgba(75,192,192,1)",
+              "#ecf0f1",
+              "#50AF95",
+              "#f3ba2f",
+              "#2a71d0",
+            ],
+            borderColor: "black",
+            borderWidth: 2,
+          },
+        ],
+      });
+
     return <Line data={UserData} />
 }
--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,6 @@ import Button from "../components/button";
 import LineChart from "../components/line-chart";
 
 export default function Homepage() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
   return (
     <div>
       <Button />
