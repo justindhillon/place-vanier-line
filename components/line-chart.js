f21ab30f65578b481f9440debc1f367c54f88a6c
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-22 11:52:01 -0700

    modified:   components/line-chart.js

--- components/line-chart.js
+++ components/line-chart.js
@@ -6,11 +6,7 @@ import { Chart as ChartJS } from 'chart.js/auto'
 export default function LineChart () {
     fetch('https://placevanierline.vercel.app/api/get-data', { method: 'POST' })
     .then(response => response.json())
-    .then(response => {
-      for (let i = 0; i <= 95; i++) {
-        console.log(response.pets.rows.i)
-      }
-    }); 
+    .then(response => console.log(response.pets.rows)); 
 
     const UserData = [
         {
