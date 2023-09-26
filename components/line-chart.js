e0384dc65579827db0a815290a0c3e1e6f943304
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-25 19:46:22 -0700

    modified:   components/line-chart.js

--- components/line-chart.js
+++ components/line-chart.js
@@ -9,7 +9,27 @@ export default function LineChart () {
   useEffect( () => { 
       async function fetchData() {
           const res = await axios.get('https://placevanierline.vercel.app/api/get-data'); 
-          setPosts(res.data.pets.rows);
+
+          const data = {
+            labels: res.data.pets.rows.map((info) => info.time),
+            datasets: [
+              {
+                label: "Users Gained",
+                data: res.data.pets.rows.map((info) => info.value),
+                backgroundColor: [
+                  "rgba(75,192,192,1)",
+                  "#ecf0f1",
+                  "#50AF95",
+                  "#f3ba2f",
+                  "#2a71d0",
+                ],
+                borderColor: "black",
+                borderWidth: 2,
+              },
+            ],
+          }
+
+          setPosts(data);
       }
       fetchData();
   }, []);
