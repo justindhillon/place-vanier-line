05d7d90ef23d968485f3e5fc260ad7e751f630e8
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-25 19:06:40 -0700

    modified:   components/line-chart.js

--- components/line-chart.js
+++ components/line-chart.js
@@ -4,72 +4,15 @@ import { Line } from 'react-chartjs-2'
 import { Chart as ChartJS } from 'chart.js/auto'
 
 export default function LineChart () {
-    /*const UserData = [
-        {
-          id: 1,
-          year: 2016,
-          userGain: 80000,
-          userLost: 823,
-        },
-        {
-          id: 2,
-          year: 2017,
-          userGain: 45677,
-          userLost: 345,
-        },
-        {
-          id: 3,
-          year: 2018,
-          userGain: 78888,
-          userLost: 555,
-        },
-        {
-          id: 4,
-          year: 2019,
-          userGain: 90000,
-          userLost: 4555,
-        },
-        {
-          id: 5,
-          year: 2020,
-          userGain: 4300,
-          userLost: 234,
-        },
-      ];
-
-      const [userData, setUserData] = useState({
-        labels: UserData.map((data) => data.year),
-        datasets: [
-          {
-            label: "Users Gained",
-            data: UserData.map((data) => data.userGain),
-            backgroundColor: [
-              "rgba(75,192,192,1)",
-              "#ecf0f1",
-              "#50AF95",
-              "#f3ba2f",
-              "#2a71d0",
-            ],
-            borderColor: "black",
-            borderWidth: 2,
-          },
-        ],
-      });*/
-
-  const [users, setUsers] = useState([]);
-
-  function fetchData() {
-    const res = fetch('https://placevanierline.vercel.app/api/get-data');
-    setUsers(res);
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
+  const [posts, setPosts] = useState([]);
   
-  return (
-    <div>
-      <p>{users}</p>
-    </div>
-  );
+  useEffect( () => { 
+      async function fetchData() {
+          const res = await axios.get('https://placevanierline.vercel.app/api/get-data'); 
+          setPosts(res.pets);
+      }
+      fetchData();
+  }, []);
+  
+  return <div>{posts}</div>
 }
