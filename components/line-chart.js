283cdf02c7bc73e61fcf7830618bef5ccad9f5a0
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-25 19:30:11 -0700

    modified:   components/line-chart.js

--- components/line-chart.js
+++ components/line-chart.js
@@ -5,25 +5,34 @@ import { Line } from 'react-chartjs-2'
 import { Chart as ChartJS } from 'chart.js/auto'
 
 export default function LineChart () {
-  const [posts, setPosts] = useState([]);
+  const [data, setData] = useState([]);
   
   useEffect( () => { 
       async function fetchData() {
           const res = await axios.get('https://placevanierline.vercel.app/api/get-data'); 
-          setPosts(res.data.pets.rows);
+          setData(res.data.pets.rows);
       }
       fetchData();
   }, []);
+
+  const [finalData, setFinalData] = useState({
+    labels: data.map((info) => info.time),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: data.map((info) => info.value),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
   
-  return (
-    <div>
-      {posts.length > 0 && (
-        <ul>
-          {posts.map(user => (
-            <li>{user.time}{user.value}</li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+  return <Line data={finalData} />
 }
