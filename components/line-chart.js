93fdd26ebd12d52f0d4d8e7892b2bad758a3dde9
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-25 19:32:12 -0700

    modified:   components/line-chart.js

--- components/line-chart.js
+++ components/line-chart.js
@@ -13,26 +13,26 @@ export default function LineChart () {
           setData(res.data.pets.rows);
       }
       fetchData();
-  }, []);
 
-  const [finalData, setFinalData] = useState({
-    labels: data.map((info) => info.time),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: data.map((info) => info.value),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
+      const [finalData, setFinalData] = useState({
+        labels: data.map((info) => info.time),
+        datasets: [
+          {
+            label: "Users Gained",
+            data: data.map((info) => info.value),
+            backgroundColor: [
+              "rgba(75,192,192,1)",
+              "#ecf0f1",
+              "#50AF95",
+              "#f3ba2f",
+              "#2a71d0",
+            ],
+            borderColor: "black",
+            borderWidth: 2,
+          },
         ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
+      });
+  }, []);
   
   return <Line data={finalData} />
 }
