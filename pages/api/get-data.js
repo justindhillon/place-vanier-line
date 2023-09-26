d73c52c0984518c54bb75546b23acc7dcae57897
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-26 07:56:40 -0700

    modified:   pages/api/get-data.js

--- pages/api/get-data.js
+++ pages/api/get-data.js
@@ -1,6 +1,6 @@
 import { sql } from '@vercel/postgres';
 
 export default async function handler(request, response) {
-    const pets = await sql`SELECT * FROM Database;`;
+    const pets = await sql`SELECT * FROM Database ORDER BY time ASC;;`;
     return response.status(200).json({ pets });
 }
