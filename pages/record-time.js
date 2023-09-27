a4f940234256b4f5d2913526aa3d316d4a486b74
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-27 10:20:47 -0700

    new file:   pages/record-time.js

---
+++ pages/record-time.js
@@ -0,0 +1,12 @@
+import Button from "../components/button";
+import Navbar from '../components/navbar';
+
+export default function Homepage() {
+  return (
+    <div>
+      <Navbar />
+      <br />
+      <Button />
+    </div>
+  );
+}
