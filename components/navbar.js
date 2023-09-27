9aff72b4c23a88b2cab97ef1ed3a96979dfea75b
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-26 18:54:29 -0700

    modified:   components/navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import React, { useState } from "react";
-import NavItem from "";
+import NavItem from "../components/nav-item.js";
 
 const MENU_LIST = [
   { text: "Home", href: "/" },
