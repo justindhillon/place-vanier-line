d1c9ec6b9a88cfb378dc6172f285dcdf3e966069
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-27 10:54:26 -0700

    modified:   components/navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -4,7 +4,7 @@ import NavItem from "../components/nav-item.js";
 
 const MENU_LIST = [
   { text: "Graph", href: "/" },
-  { text: "</> Source Code", href: "https://github.com/justindhillon/place-vanier-rush-hour" },
+  { text: "</> Source Code", href: "https://github.com/justindhillon/place-vanier-line" },
   { text: "Twitter", href: "https://twitter.com/justindhillon0" },
   { text: "Email", href: "https://mail.google.com/mail/u/?authuser=justin.singh.dhillon@gmail.com" },
 ];
