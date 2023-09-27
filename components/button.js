fb8893dbce79cab380fe84e3c0c4f4b172ee08c1
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-27 10:49:50 -0700

    modified:   components/button.js

--- components/button.js
+++ components/button.js
@@ -7,7 +7,11 @@ export default function Button() {
   
     return (
       <div class="button-container">
+        <br />
+        <br />
         <h1>Stuck in line?</h1>
+        <br />
+        <br />
         <button class={styles.button} onClick={handleClick}>PRESS ME</button>
       </div>
     );
