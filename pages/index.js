a9805c917fe3c1f521d8b03d69a829044202a6f7
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-26 18:48:33 -0700

    modified:   pages/index.js 	modified:   styles/global.css

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import { useState } from "react";
 import Button from "../components/button";
+import Navbar from '../components/navbar';
 import LineChart from "../components/line-chart";
 
 export default function Homepage() {
   return (
     <div>
-      <br />
-      <Button />
+      <Navbar />
       <br />
       <LineChart />
     </div>
