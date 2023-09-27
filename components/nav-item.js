d19fb8e58904dca336ec16508b8d5dcaded0464b
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-26 18:48:57 -0700

    new file:   components/nav-item.js 	new file:   components/navbar.js

---
+++ components/nav-item.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function NavItem ({ text, href, active }) {
+  return (
+    <Link href={href}>
+      <a
+        className={`nav__item ${
+          active ? "active" : ""
+        }`}
+      >
+        {text}
+      </a>
+    </Link>
+  );
+}
---
+++ components/navbar.js
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import Logo from "./Logo";
+import NavItem from "";
+
+const MENU_LIST = [
+  { text: "Home", href: "/" },
+  { text: "</> Source Code", href: "https://github.com/justindhillon/place-vanier-rush-hour" },
+  { text: "Twitter", href: "https://twitter.com/justindhillon0" },
+  { text: "Gmail", href: "justin.singh.dhillon@gmail.com" },
+];
+
+export default function Navbar() {
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
+
+  return (
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"}>
+          <a>
+            <h1 className="logo">PlaceVanierLine</h1>
+          </a>
+        </Link>
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
