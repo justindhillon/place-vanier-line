a28b814709a4d19c417c8ae5d87b00482be8a50a
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-26 19:20:37 -0700

    modified:   components/line-chart.js

--- components/line-chart.js
+++ components/line-chart.js
@@ -30,6 +30,6 @@ export default function LineChart () {
   }
 
   return (
-    <Line data={data} width={"30%"} />
+    <Line data={data} height="200px" width="200px" options={{ maintainAspectRatio: false }} />
   )
 }
