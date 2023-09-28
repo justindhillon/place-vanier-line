9ec1dd42d749b185a2ae3e224aaa4691bf9a8302
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-28 13:49:42 -0700

    modified:   components/about-website.js

--- components/about-website.js
+++ components/about-website.js
@@ -1,10 +1,14 @@
 export default function AboutWebsite() {
     return (
-        <p>
-            This project is created with the goal of solving rush hour at place vanier.
-            There is a poster at the place vanier food hall, which you can scan when you are stuck in line.
-            Once recorded, it will update the graph on the website, so others can see when it is the most busy.
-            With that knowlage we can solve world hunger (or get our food sooner)
-        </p>
+        <div>
+            <br />
+            <p>
+                This project is created with the goal of solving rush hour at place vanier.
+                There is a poster at the place vanier food hall, which you can scan when you are stuck in line.
+                Once recorded, it will update the graph on the website, so others can see when it is the most busy.
+                With that knowlage we can solve world hunger (or get our food sooner)
+            </p>
+            <br />
+        </div>
     )
 }
