c96e516bc76ea692872d64640c70eb301bf9f3e4
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-28 13:59:00 -0700

    modified:   components/navbar.js 	deleted:    components/navbar.module.css 	modified:   styles/global.css

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import NavItem from "../components/nav-item.js";
-import styles from './navbar.module.css'
 
 const MENU_LIST = [
   { text: "Graph", href: "/" },
