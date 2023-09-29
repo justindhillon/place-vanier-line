2dd3f783ab1244d3e92d4de93e6a2a091da5bc05
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-28 17:30:58 -0700

    modified:   components/about-website.js 	deleted:    components/footer.js 	modified:   styles/global.css

--- components/about-website.js
+++ components/about-website.js
@@ -1,16 +1,23 @@
 export default function AboutWebsite() {
     return (
-        <div style={{background: "#f1f1f1"}}>
-            <br />
-            <br />
-            <p style={{width: "90%", margin: "auto"}}>
-                This project is created with the goal of solving rush hour at place vanier.
-                There is a poster at the place vanier food hall, which you can scan when you are stuck in line.
-                Once recorded, it will update the graph on the website, so others can see when it is the most busy.
-                With that knowlage we can solve world hunger (or get our food sooner)
-            </p>
-            <br />
-            <br />
+        <div id="bottom-stuff">
+            <div id="search">
+                <br />
+                <br />
+                <p style={{width: "90%", margin: "auto"}}>
+                    This project is created with the goal of solving rush hour at place vanier.
+                    There is a poster at the place vanier food hall, which you can scan when you are stuck in line.
+                    Once recorded, it will update the graph on the website, so others can see when it is the most busy.
+                    With that knowlage we can solve world hunger (or get our food sooner)
+                </p>
+                <br />
+                <br />
+            </div>
+
+            <div id="bottom">
+                <p>Learn More <a href="https://placevanierline.vercel.app/about-me">About Me</a></p>
+                <br />
+            </div>
         </div>
     )
 }
