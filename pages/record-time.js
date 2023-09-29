124bb37b3f97ecf7ecfe3334d28ab36567768be2
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-28 21:33:37 -0700

    modified:   pages/record-time.js

--- pages/record-time.js
+++ pages/record-time.js
@@ -1,6 +1,7 @@
 import AboutWebsite from "../components/about-website";
 import Button from "../components/button";
 import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
 export default function Homepage() {
   return (
