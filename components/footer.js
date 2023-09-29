db45fc38317b24010366ea2c5cd6848ff0f2f720
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-28 21:31:24 -0700

    new file:   components/footer.js

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <div>
+            <br />
+            <br />
+            <p>Made with <span style={{color:"#2596be"}}>♥</span> by <a href="https://placevanierline.vercel.app/about-me"><span style={{color:"#2596be"}}>Justin Dhillon</span></a></p>
+            <br />
+            <br />
+        </div>
+    )
+}
