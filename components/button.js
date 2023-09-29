02fbc5672d8371e68c8c89c84870b2fd6f3163f5
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-09-28 21:16:53 -0700

    modified:   components/button.js

--- components/button.js
+++ components/button.js
@@ -6,14 +6,17 @@ export default function Button() {
     }
   
     return (
-      <div class={styles.buttonContainer}>
-        <br /><br /><br /><br />
-        <h1 style={{fontSize: "3em"}}>Stuck in line?</h1>
-        <br /><br />
-        <a href='https://placevanierline.vercel.app/'>
-          <button class={styles.button} onClick={handleClick}>PRESS ME</button>
-        </a>
-        <br /><br /><br /><br /><br />
+      <div>
+        <br />
+        <div class={styles.buttonContainer}>
+          <br /><br /><br />
+          <h1 style={{fontSize: "3em"}}>Stuck in line?</h1>
+          <br /><br />
+          <a href='https://placevanierline.vercel.app/'>
+            <button class={styles.button} onClick={handleClick}>PRESS ME</button>
+          </a>
+          <br /><br /><br /><br /><br />
+        </div>
       </div>
     );
 }
