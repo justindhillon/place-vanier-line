16248eebecc295638a39d8d3dee5237241da9d26
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-10-02 19:27:14 -0700

    modified:   components/about-me.js

--- components/about-me.js
+++ components/about-me.js
@@ -6,23 +6,27 @@ export default function AboutMe() {
         <div>
             <br /><br />
             <table style={{background:"#f1f1f1"}} class="about-me">
+                <br />
                 <tr>
                     <td>
-                        <Image class="image" src={profile} alt="Profile" />;
+                        <Image class="image" src={profile} alt="Profile" />
                     </td>
                     <td style={{textAlign:"left"}}>
                         <h1 style={{color:"#2596be"}}>Justin Dhillon</h1>
+                        <br />
                         <ul>
                             <li>🎓 University of British Columbia CPSC 2028</li>
-                            <li><a style={{color:"#2596be"}} href='https://github.com/justindhillon/place-vanier-line'>&lt;/&gt; Source Code</a></li>
                             <li>🧑‍💻 Computer Science Nerd</li>
-                            <li><a style={{color:"#2596be"}} href='https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=justin.singh.dhillon@gmail.com'>📫 Email</a></li>
                             <li>🐧Linux Enthusiast</li>
-                            <li><a style={{color:"#2596be"}} href='https://twitter.com/justindhillon0'>🐦 Twitter</a></li>
                             <li>🇨🇦 Vancouver, Canada</li>
+                            <br />
+                            <li><a style={{color:"#2596be"}} href='https://github.com/justindhillon/place-vanier-line'>&lt;/&gt; Source Code</a></li>
+                            <li><a style={{color:"#2596be"}} href='https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=justin.singh.dhillon@gmail.com'>📫 Email</a></li>
+                            <li><a style={{color:"#2596be"}} href='https://twitter.com/justindhillon0'>🐦 Twitter</a></li>
                         </ul>
                     </td>
                 </tr>
+                <br />
             </table>
             <div style={{textAlign:"center"}}>
                 <br /><br />
