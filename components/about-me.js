15d358acc344633bf408a65c288354772b1ca72b
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-10-02 19:35:12 -0700

    modified:   components/about-me.js 	modified:   styles/global.css

--- components/about-me.js
+++ components/about-me.js
@@ -6,29 +6,27 @@ export default function AboutMe() {
         <div>
             <br />
             <br />
-            <table style={{background:"#f1f1f1"}} class="about-me">
+            <div style={{background:"#f1f1f1"}} class="about-me">
                 <br />
-                <tr>
-                    <td>
-                        <Image class="image" src={profile} alt="Profile" />
-                    </td>
-                    <td style={{textAlign:"left"}}>
-                        <h1 style={{color:"#2596be"}}>Justin Dhillon</h1>
+                <div>
+                    <Image class="image" src={profile} alt="Profile" />
+                </div>
+                <div style={{textAlign:"left"}}>
+                    <h1 style={{color:"#2596be"}}>Justin Dhillon</h1>
+                    <br />
+                    <ul>
+                        <li>🎓 University of British Columbia CPSC 2028</li>
+                        <li>🧑‍💻 Computer Science Nerd</li>
+                        <li>🐧Linux Enthusiast</li>
+                        <li>🇨🇦 Vancouver, Canada</li>
                         <br />
-                        <ul>
-                            <li>🎓 University of British Columbia CPSC 2028</li>
-                            <li>🧑‍💻 Computer Science Nerd</li>
-                            <li>🐧Linux Enthusiast</li>
-                            <li>🇨🇦 Vancouver, Canada</li>
-                            <br />
-                            <li><a style={{color:"#2596be"}} href='https://github.com/justindhillon/place-vanier-line'>&lt;/&gt; Source Code</a></li>
-                            <li><a style={{color:"#2596be"}} href='https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=justin.singh.dhillon@gmail.com'>📫 Email</a></li>
-                            <li><a style={{color:"#2596be"}} href='https://twitter.com/justindhillon0'>🐦 Twitter</a></li>
-                        </ul>
-                    </td>
-                </tr>
+                        <li><a style={{color:"#2596be"}} href='https://github.com/justindhillon/place-vanier-line'>&lt;/&gt; Source Code</a></li>
+                        <li><a style={{color:"#2596be"}} href='https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=justin.singh.dhillon@gmail.com'>📫 Email</a></li>
+                        <li><a style={{color:"#2596be"}} href='https://twitter.com/justindhillon0'>🐦 Twitter</a></li>
+                    </ul>
+                </div>
                 <br />
-            </table>
+            </div>
             <div style={{textAlign:"center"}}>
                 <br /><br />
                 <p>Back to <a href="https://placevanierline.vercel.app/"><span style={{color:"#2596be"}}>Graph</span></a></p>
