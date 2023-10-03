60fc7d552bb4384b3c01ef65f384e93570309495
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-10-03 10:51:01 -0700

    modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import Navbar from '../components/navbar';
 import LineChart from "../components/line-chart";
 import AboutWebsite from '../components/about-website';
 import Footer from '../components/footer';
+import Head from 'next/head';
 
 export default function Homepage() {
   return (
     <div>
       <Head>
-      <meta name="google-site-verification" content="59zk-yCC5eo9gEGfyEefz3nXxRqeDVqIMpB150ddFBk" />
+        <meta name="google-site-verification" content="59zk-yCC5eo9gEGfyEefz3nXxRqeDVqIMpB150ddFBk" />
       </Head>
       <div>
         <Navbar />
