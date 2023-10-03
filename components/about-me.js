b8bd4c995256d24952e35c3546711a6981e7738e
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-10-02 18:39:45 -0700

    modified:   components/about-me.js

--- components/about-me.js
+++ components/about-me.js
@@ -9,7 +9,15 @@ export default function AboutMe() {
                         <img src={profile} alt="Profile" />;
                     </div>
                     <div>
-
+                        <h1>Justin Dhillon</h1>
+                        <ul>
+                            <li>👋 Hi, I’m @justindhillon</li>
+                            <li>🎓 University of British Columbia 2028</li>
+                            <li>🧑‍💻 Computer Science Nerd</li>
+                            <li>🐧Linux Enthusiast</li>
+                            <li>🇨🇦 Vancouver, Canada</li>
+                            <li>📫 justin.singh.dhillon@gmail.com</li>
+                        </ul>
                     </div>
                 </tr>
             </table>
