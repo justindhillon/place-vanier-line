59b0d125400d02029ed0b082db0df995ca5e4665
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-10-02 19:22:45 -0700

    modified:   components/about-me.js

--- components/about-me.js
+++ components/about-me.js
@@ -14,12 +14,12 @@ export default function AboutMe() {
                         <h1 style={{color:"#2596be"}}>Justin Dhillon</h1>
                         <ul>
                             <li>🎓 University of British Columbia CPSC 2028</li>
+                            <li><a style={{color:"#2596be"}} href='https://github.com/justindhillon/place-vanier-line'>&lt;/&gt; Source Code</a></li>
                             <li>🧑‍💻 Computer Science Nerd</li>
+                            <li><a style={{color:"#2596be"}} href='https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=justin.singh.dhillon@gmail.com'>📫 Email</a></li>
                             <li>🐧Linux Enthusiast</li>
+                            <li><a style={{color:"#2596be"}} href='https://twitter.com/justindhillon0'>🐦 Twitter</a></li>
                             <li>🇨🇦 Vancouver, Canada</li>
-                            <li><a href='https://github.com/justindhillon/place-vanier-line'>&lt;/&gt; Source Code</a></li>
-                            <li><a href='https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=justin.singh.dhillon@gmail.com'>📫 Email</a></li>
-                            <li><a href='https://twitter.com/justindhillon0'>Twitter</a></li>
                         </ul>
                     </td>
                 </tr>
