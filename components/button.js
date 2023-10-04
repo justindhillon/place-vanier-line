0e3fe0d8def5340bce5b73f9b256eab2025dac1d
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-10-04 14:47:21 -0700

    modified:   components/button.js

--- components/button.js
+++ components/button.js
@@ -8,7 +8,7 @@ export default function Button() {
 
     NextResponse.cookies.set('time', '15:00');
 
-    console.log(NextRequest.cookies.get('time')?.value);
+    //console.log(NextRequest.cookies.get('time')?.value);
   
     return (
       <div>
