185d0156ea470c155379fe057b9168cbf6a0a834
Justin Dhillon <justin.singh.dhillon@gmail.com>
2023-10-04 18:52:30 -0700

    modified:   components/button.js 	modified:   pages/api/add.js

--- pages/api/add.js
+++ pages/api/add.js
@@ -1,7 +1,31 @@
 import { sql } from '@vercel/postgres';
+
+function getCurrentTime(separator=''){
+    let newDate = new Date()
+    let hour = newDate.getUTCHours() + 17;
+    let minute = newDate.getMinutes();
+  
+    minute = (Math.round(minute/15) * 15);
+
+    if (minute == 60) {
+        minute = 0;
+        hour++;
+    }
+
+    hour = hour % 24;
+
+    if (hour < 7 || 21 < hour) {
+        return "invalid";
+    }
+
+    let formattedHour = hour.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
+    let formattedMinute = minute.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })
+    
+    return formattedHour.toString() + ":" + formattedMinute.toString();
+}
  
 export default async function handler(request, response) {
-    let time = request.body;
+    let time = getCurrentTime();
 
     if (time != "invalid") { 
         await sql`UPDATE Database SET Value = Value::DECIMAL + 1 WHERE Time = ${time};`;
